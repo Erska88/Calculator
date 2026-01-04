049456fa1f8e56a1249e84ea5a5fade1bd7c226c
Eerno Niemenmaa <eerno.niemenmaa@gmail.com>
2026-01-04 08:19:22 +0200

    sec com

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 
 
 print("Hello, World!")
+
+def calculator_temp():
+
+    a = input("First input: ")
+    b = input("Second input: ")
+
+    output = f"{a} + {b} = {float(a) + float(b)}"
+    print(output)
+
+
+calculator_temp()
